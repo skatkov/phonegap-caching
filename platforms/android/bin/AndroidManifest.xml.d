89ce64c30e96309588477fa803060500b55af193
SK <lunaticman@gmail.com>
2013-09-12 04:40:45 +0700

    initial something

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/lun/code/phonegap-caching/platforms/android/bin/AndroidManifest.xml : \
+/Users/lun/code/phonegap-caching/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/lun/code/phonegap-caching/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
+/Users/lun/code/phonegap-caching/platforms/android/bin/HelloWorld.ap_ \
+/Users/lun/code/phonegap-caching/platforms/android/bin/classes.dex \
+/Users/lun/code/phonegap-caching/platforms/android/libs/cordova-3.0.0.jar \
