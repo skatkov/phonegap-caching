e2872b2d54be50ce1de484867f01695822ec513a
SK <lunaticman@gmail.com>
2013-09-16 05:18:03 +0700

    compare local sfv file with remote

--- platforms/android/src/com/phonegap/hello_world/FileChecker.java
+++ platforms/android/src/com/phonegap/hello_world/FileChecker.java
@@ -5,14 +5,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
-import android.util.Log;
 
 public class FileChecker {
+	
+	public HashMap <String, String> localSfv = null;
+	
 
-    public boolean notEmpty() {
-        return true;
+    public FileChecker(HashMap<String, String> localSfv) {
+    	this.localSfv = localSfv;
     }
     
     public static HashMap<String, String> sfvToHash(File sfvFile){
@@ -45,4 +53,33 @@ public class FileChecker {
     	
 		return hash;
     }
+
+	public void updateLocal(HashMap<String, String> remoteHash) {
+		Iterator<String> result = compareEntries(this.localSfv, remoteHash).iterator();
+		while (result.hasNext()){
+			// remove keys that have different checksum
+			this.localSfv.remove(result.next());
+		}
+	}
+	
+	public static <K extends Comparable<? super K>, V>
+	List<String> compareEntries(final Map<K, V> map1,
+	    final Map<K, V> map2){
+		//return list of keys that don't different checksum
+	    final Collection<K> allKeys = new HashSet<K>();
+	    allKeys.addAll(map1.keySet());
+	    allKeys.addAll(map2.keySet());
+	    List<String> result = new ArrayList<String>();
+	    for(final K key : allKeys){
+	    	if (!(map1.containsKey(key) == map2.containsKey(key) &&
+	            Boolean.valueOf(equal(map1.get(key), map2.get(key))))){
+	    		result.add((String) key);
+	    	}
+	    }
+	    return result;
+	}
+
+	private static boolean equal(final Object obj1, final Object obj2){
+	    return obj1 == obj2 || (obj1 != null && obj1.equals(obj2));
+	}
 }
--- platforms/android/src/com/phonegap/hello_world/FileCheckerTest.java
+++ platforms/android/src/com/phonegap/hello_world/FileCheckerTest.java
@@ -2,24 +2,52 @@ package com.phonegap.hello_world;
 
 import static org.junit.Assert.*;
 import static com.phonegap.hello_world.FileChecker.sfvToHash;
+import static com.phonegap.hello_world.FileChecker.compareEntries;
 import java.io.File;
 import java.util.HashMap;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class FileCheckerTest {
+	final File localSfv = new File("./assets/sfv/local.sfv");
+	FileChecker sfv = null;
+	
+	@Before
+	public void setUp(){
+		this.sfv = new FileChecker(sfvToHash(this.localSfv));
+	}
+	
 	@Test
-	public void testNotEmpty() {
-		FileChecker sfv = new FileChecker();
-        assertTrue(sfv.notEmpty());
+	public void testConstructor() {
+		assertEquals("FB524549", this.sfv.localSfv.get("poker/audio/slots/spin_2.mp3"));
 	}
 	
 	@Test
 	public void testSfv(){
-		File sfv = new File("./assets/sfv/local.sfv");
-		HashMap<String, String> result = sfvToHash(sfv);
+		HashMap<String, String> result = sfvToHash(localSfv);
 		assertTrue(result.containsKey("poker/audio/slots/spin_2.mp3"));
 		assertEquals("FB524549", result.get("poker/audio/slots/spin_2.mp3"));
 	}
+	
+	@Test
+	public void compareEntriesTest(){
+		assertTrue(1 == compareEntries(sfv.localSfv, changeHash()).size());
+	}
+	
+	@Test
+	public void compareWithLocal(){
+		sfv.updateLocal(changeHash());
+		
+		assertNull(sfv.localSfv.get("poker/audio/slots/raise_increment.mp3"));
+	}
 
+	private HashMap<String, String> changeHash() {
+		HashMap<String, String> updatedHash = sfvToHash(localSfv);
+		updatedHash.remove("poker/audio/slots/raise_increment.mp3");
+		updatedHash.put("poker/audio/slots/raise_increment.mp3", "HAXOR12");
+		return updatedHash;
+	}
+
+	
+	
 }
